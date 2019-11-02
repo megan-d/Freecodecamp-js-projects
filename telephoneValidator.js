58de17e30328c03d05950127c42f593be508b304
Megan D <45672063+megan-d@users.noreply.github.com>
2019-11-02 14:06:53 -0600

    Begin regular expression for telephone check

--- telephoneValidator.js
+++ telephoneValidator.js
@@ -1,6 +1,45 @@
 function telephoneCheck(str) {
   // Good luck!
-  return true;
+  const re = /^1?\s?[0-9]{3}-?\s?[0-9]{3}-?\s?[0-9]{4}$/g;
+  if (re.test(str)) {
+    return true;
+  }
+  return false;
 }
 
 telephoneCheck('555-555-5555');
+
+// 555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+
+// telephoneCheck("555-555-5555") should return a boolean.
+// telephoneCheck("1 555-555-5555") should return true.
+// telephoneCheck("1 (555) 555-5555") should return true.
+// telephoneCheck("5555555555") should return true.
+// telephoneCheck("555-555-5555") should return true.
+// telephoneCheck("(555)555-5555") should return true.
+// telephoneCheck("1(555)555-5555") should return true.
+// telephoneCheck("555-5555") should return false.
+// telephoneCheck("5555555") should return false.
+// telephoneCheck("1 555)555-5555") should return false.
+// telephoneCheck("1 555 555 5555") should return true.
+// telephoneCheck("1 456 789 4444") should return true.
+// telephoneCheck("123**&!!asdf#") should return false.
+// telephoneCheck("55555555") should return false.
+// telephoneCheck("(6054756961)") should return false
+// telephoneCheck("2 (757) 622-7382") should return false.
+// telephoneCheck("0 (757) 622-7382") should return false.
+// telephoneCheck("-1 (757) 622-7382") should return false
+// telephoneCheck("2 757 622-7382") should return false.
+// telephoneCheck("10 (757) 622-7382") should return false.
+// telephoneCheck("27576227382") should return false.
+// telephoneCheck("(275)76227382") should return false.
+// telephoneCheck("2(757)6227382") should return false.
+// telephoneCheck("2(757)622-7382") should return false.
+// telephoneCheck("555)-555-5555") should return false.
+// telephoneCheck("(555-555-5555") should return false.
+// telephoneCheck("(555)5(55?)-5555") should return false
