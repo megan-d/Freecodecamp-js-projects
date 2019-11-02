c9c550c0a0d59c0a8e478fe90431b2ed62a5a374
Megan D <45672063+megan-d@users.noreply.github.com>
2019-11-01 21:56:08 -0600

    Complete caesars cipher challenge

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,29 +1,26 @@
 function rot13(str) {
   // LBH QVQ VG!
-  // 1. Using loop, get ASCII value of each letter in string using str.charCodeAt and push that value into a new array
+  // Using loop, get ASCII value of each letter in string and push that value into a new array
   const asciArr = [];
   for (let i = 0; i < str.length; i++) {
     asciArr.push(str.charCodeAt(i));
   }
-  console.log({ asciArr });
-
-  // 2. Loop through array of asci values. Say if value is within range of A-Z ascii (65 - 90), shift that value by 13. Otherwise, just keep that value the same (punctuation and spaces).
-  // NEED TO FIGURE OUT HOW TO DEAL WITH ONCE GET TO Z GOING BACK TO A. SOMETHING TO DO WITH %.
+  // Use map to loop through array of ASCII values and make appropriate shifts if value falls between A-Z.
   const cipherArr = asciArr.map(function(el) {
-    if (el >= 65 && el <= 90) {
-      return (el += 13);
+    let element = el;
+    if (el >= 65 && el <= 77) {
+      return (element += 13);
     }
-    return (el = el);
+    if (el > 77 && el <= 90) {
+      return (element = 13 - (90 - el) + 64);
+    }
+    return el;
   });
-  console.log({ cipherArr });
-
-  // 3. Convert final array back to letters from ascii using String.fromCharCode
+  // Convert final array back to letters from ASCII
   const finalArr = cipherArr.map(function(el) {
     return String.fromCharCode(el);
   });
-  console.log({ finalArr });
-
-  // 4. Use join method to bring string back together
+  // Use join method to bring string back together
   const newStr = finalArr.join('');
 
   return newStr;
